aed5ea7e37f1aae94f2671fdb145a67de20782f5
Sujan Rana <khembikram1@gmail.com>
2024-05-31 06:50:51 +0545

    revision for the first class in java'

---
+++ revision1_java.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * revision1_java
+ */
+public class revision1_java {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("The  number is even");
+        }else{
+            System.out.println("The number is odd");
+        }
+
+        scanner.close();
+    }
+}

4ee671a35229bbd3c35c5a7995d1a22f810cf132
Sujan Rana <khembikram1@gmail.com>
2024-06-02 07:14:20 +0545

    IpByDomain

--- Unit2/IpByDomain.java
+++ Unit2/IpByDomain.java
@@ -1,5 +1,15 @@
 package Unit2;
 
+import java.net.*;
+
 public class IpByDomain {
-    
+
+    public static void main(String[] args) {
+        try {
+            InetAddress address = InetAddress.getByName("www.traveltaletaste.com");
+            System.out.println(address);
+        } catch (UnknownHostException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }

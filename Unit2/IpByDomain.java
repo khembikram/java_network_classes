f0fbcfabfcacc6979ce76efb64f8c3ef588cccca
Sujan Rana <khembikram1@gmail.com>
2024-06-02 07:27:17 +0545

    Ip of address and host

--- Unit2/IpByDomain.java
+++ Unit2/IpByDomain.java
@@ -6,8 +6,13 @@ public class IpByDomain {
 
     public static void main(String[] args) {
         try {
-            InetAddress address = InetAddress.getByName("www.traveltaletaste.com");
+            InetAddress address = InetAddress.getByName("www.khembikram.com.np");
+            // prints ipaddress of the domain
             System.out.println(address);
+            // Address IP
+            System.out.println("Address:" + address.getHostAddress());
+            // Host Ip
+            System.out.println("Name:" + address.getHostAddress() );
         } catch (UnknownHostException ex) {
             System.out.println(ex.getMessage());
         }

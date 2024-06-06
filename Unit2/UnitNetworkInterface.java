0613065fa1ac6d63f7de68dd90456e3cb8650e52
Sujan Rana <khembikram1@gmail.com>
2024-06-06 06:54:05 +0545

    interface solved

--- Unit2/UnitNetworkInterface.java
+++ Unit2/UnitNetworkInterface.java
@@ -3,7 +3,11 @@ import java.net.SocketException;
 import java.util.Enumeration;
 
 public class UnitNetworkInterface {
-    public static void main(String[] args) {
-        Enumeration<NetworkInterface> interface = NetworkInterface.getNetworkInterfaces();
+    public static void main(String[] args) throws SocketException {
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while(interfaces.hasMoreElements()){
+            NetworkInterface ni = interfaces.nextElement();
+            System.err.println(ni);
+        }
     }
 }

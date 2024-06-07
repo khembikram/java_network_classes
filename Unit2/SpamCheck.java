8403fac07fa5e6c20054c92a5723a13deda737ef
Sujan Rana <khembikram1@gmail.com>
2024-06-07 07:11:38 +0545

    spam checker final

--- Unit2/SpamCheck.java
+++ Unit2/SpamCheck.java
@@ -1,14 +1,36 @@
 
-public  class  SpamCheck{
-    public static  final String BLACKHOLE = "sbl.spamhaus.org";
-    
-    public static void main(String[] args) {
-        for(String arg: args) {
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class SpamCheck {
+
+    public static final String BLACKHOLE = "sbl.spamhaus.org";
+
+    public static void main(String[] args) throws UnknownHostException {
+        for (String arg : args) {
             if (isSpammer(arg)) {
                 System.out.println(arg + " is a known Spammer.");
-            }else{
+            } else {
                 System.out.println(arg + " appears legitimate.");
             }
         }
+    }
+
+    private static boolean isSpammer(String arg) throws UnknownHostException {
+        try {
+            InetAddress address = InetAddress.getByName(arg);
+            byte[] quad = address.getAddress();
+            String query = BLACKHOLE;
+            for (byte octet : quad) {
+                int unsignedByte = octet < 0 ? octet + 256 : octet;
+                query = unsignedByte + "." + query;
+            }
+            System.err.println(query);
+            InetAddress.getByName(query);
+            return true;
+        } catch(UnknownHostException e)  {
+            return false;
+        }
+
     }
 }

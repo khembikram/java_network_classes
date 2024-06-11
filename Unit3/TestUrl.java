e7781a2f4b6329fc14606d2d343719aea1887d36
Sujan Rana <khembikram1@gmail.com>
2024-06-11 07:07:27 +0545

    command line url

--- Unit3/TestUrl.java
+++ Unit3/TestUrl.java
@@ -6,10 +6,13 @@ public class TestUrl {
 
     public static void main(String[] args) {
         try {
-            URI uri = new URI("https://www.nagarjunacollege.edu.np/");
+            String test_url = args[0]; // get the command line URL 
+            URI uri = new URI(test_url);
+            // URI uri = new URI("https://www.nagarjunacollege.edu.np/");
             URL url = uri.toURL();
             System.out.println("port : " + url.getPort());
             System.out.println("protocol :" + url.getProtocol());
+            System.out.println("path :" + url.getPath());
         } catch (MalformedURLException e) {
             System.out.println(e.getMessage());
         } catch (URISyntaxException ex) {

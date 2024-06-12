b31416f1b6971e7dc8d168615e7d5701d857a03c
Sujan Rana <khembikram1@gmail.com>
2024-06-12 06:46:13 +0545

    protocol test

---
+++ Unit3/ProtocolTester.java
@@ -0,0 +1,11 @@
+public class ProtocolTester {
+    public static void main(String[] args) {
+        String []urls= {
+            "https://example.com",
+            "http://example.com"
+        };
+        for(String url : urls){
+            System.out.println(url);
+        }
+    }
+}

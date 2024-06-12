4d1fa8724d0ac245f7725cee5571d4d29a4358c9
Sujan Rana <khembikram1@gmail.com>
2024-06-12 06:56:27 +0545

    url update

--- Unit3/ProtocolTester.java
+++ Unit3/ProtocolTester.java
@@ -1,10 +1,32 @@
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+
 public class ProtocolTester {
+
+
+    public static void tester(String url_raw){
+        try {
+            URI uri = new URI(url_raw);
+            URL url = uri.toURL();
+            System.out.println(url.getProtocol()+" is supported");
+        } catch (MalformedURLException ex) {
+            System.out.println(ex.getMessage());
+        } catch (URISyntaxException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
-        String []urls= {
+        String[] urls = {
+            "http://adc.com",
             "https://example.com",
-            "http://example.com"
+            "file:///C:/Users/sujan/OneDrive/Pictures/Screenshots/Screenshot%202024-06-09%20224649.png"
         };
-        for(String url : urls){
+        for (String url : urls) {
             System.out.println(url);
         }
     }

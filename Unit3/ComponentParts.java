4c61f97c19be4bcd3a54a01f431de8eea01e1318
Sujan Rana <khembikram1@gmail.com>
2024-06-13 07:01:36 +0545

    by components

--- Unit3/ComponentParts.java
+++ Unit3/ComponentParts.java
@@ -1,4 +1,3 @@
-
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -7,12 +6,14 @@ import java.net.URL;
 public class ComponentParts {
     public static void main(String[] args) {
         try {
-            URI uri = new  URI("http://www.testingmcafeesites.com/index.html");
+            // URI uri = new URI("http://www.testingmcafeesites.com/index.html");
+            // http://www.testingmcafeesites.com/testcat_al.html
+            URI uri = new URI("http", "www.testingmcafeesites.com", "/testcat_al.html", null);
             URL url = uri.toURL();
             System.out.println("HOST: " + url.getHost());
         } catch (URISyntaxException e) {
             System.out.println(e.getMessage());
-        } catch (MalformedURLException ex){
+        } catch (MalformedURLException ex) {
             System.out.println(ex.getMessage());
         }
     }

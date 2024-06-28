1c78a3cd85237f323f86368ee02cd2572a1f85f1
Sujan Rana <khembikram1@gmail.com>
2024-06-28 07:08:55 +0545

    userinteraction java

---
+++ Unit5/UserInteraction.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+import java.net.*;
+
+public class UserInteraction {
+
+    public static void main(String[] args) {
+        try {
+            URI uri = new URI("https://en.wikipedia.org/wiki/Nepal");
+            URL url = uri.toURL();
+            URLConnection conn = url.openConnection();
+            if (conn.getAllowUserInteraction()){
+                System.out.println("User Interaction is allowed");
+            } else {
+                System.out.println("User interaction is not allowed");
+            }
+        } catch (URISyntaxException e) {
+            System.out.println(e.getMessage());
+        } catch (MalformedURLException ex1) {
+            System.out.println(ex1.getMessage());
+        } catch (IOException ex2) {
+            System.out.println(ex2.getMessage());
+        }
+    }
+}

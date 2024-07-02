1056f0e1a1712ee86b8f417438b1284b1eb25277
Sujan Rana <khembikram1@gmail.com>
2024-07-02 07:19:24 +0545

    client server socket

--- Unit6/ClientSide.java
+++ Unit6/ClientSide.java
@@ -1,4 +1,5 @@
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -6,7 +7,11 @@ public class ClientSide {
     public static void main(String[] args) {
         try {
             Socket client = new Socket("localhost",6667);
-
+            DataOutputStream dout = new DataOutputStream(client.getOutputStream());
+            dout.writeUTF("Hello Server");
+            dout.flush();
+            dout.close();
+            client.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
--- Unit6/ServerSide.java
+++ Unit6/ServerSide.java
@@ -5,7 +5,11 @@ public class ServerSide {
     public static void main(String[] args) {
         try {
             ServerSocket ss = new ServerSocket(6667);
-            ss.accept();
+            Socket server = ss.accept();
+            DataInputStream din = new DataInputStream(server.getInputStream());
+            String msg = din.readUTF();
+            System.out.println("Client says: " + (String) msg);
+            ss.close();
             System.out.println("Connection Established");
         } catch (IOException e) {
             System.out.println(e.getMessage());

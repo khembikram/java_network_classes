6883edbf9d9815101e5c5abfc9de348685a29ab6
Sujan Rana <khembikram1@gmail.com>
2024-07-02 07:07:33 +0545

    compiled

--- Unit6/ClientSide.java
+++ Unit6/ClientSide.java
@@ -5,8 +5,8 @@ import java.net.Socket;
 public class ClientSide {
     public static void main(String[] args) {
         try {
-            Socket client = new Socket("localhost",6666);
-            
+            Socket client = new Socket("localhost",6667);
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
--- Unit6/ServerSide.java
+++ Unit6/ServerSide.java
@@ -4,7 +4,7 @@ import java.net.*;
 public class ServerSide {
     public static void main(String[] args) {
         try {
-            ServerSocket ss = new ServerSocket(6666);
+            ServerSocket ss = new ServerSocket(6667);
             ss.accept();
             System.out.println("Connection Established");
         } catch (IOException e) {

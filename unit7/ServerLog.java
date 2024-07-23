09d4e02f4358ed0d11d37fc71056add80dbc794c
Sujan Rana <khembikram1@gmail.com>
2024-07-23 07:12:31 +0545

    Date Fetched

--- unit7/ServerLog.java
+++ unit7/ServerLog.java
@@ -2,6 +2,7 @@ package unit7;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,6 +15,12 @@ public class ServerLog {
             ServerSocket server = new ServerSocket(13);
             Socket connection = server.accept();
             infoLogger.log(Level.INFO, "Connection Established with a client");
+            Writer out = new OutputStreamWriter(connection.getOutputStream());
+            Date now = new Date();
+            out.write(now.toString() + "\r\n");
+            out.flush();
+
+            infoLogger.log(Level.INFO, "Date Written to stream");
             // System.out.println("Connection established");
             connection.close();
             server.close();

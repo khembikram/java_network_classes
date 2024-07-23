a83c49dec52a055f1ec18182f81ee53ab136de5d
Sujan Rana <khembikram1@gmail.com>
2024-07-23 06:58:47 +0545

    client and server log

---
+++ unit7/ClientLog.java
@@ -0,0 +1,16 @@
+package unit7;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientLog {
+    public static void main(String[] args) {
+        try {
+            Socket client = new Socket("localhost", 13);
+            client.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
--- unit7/ServerLog.java
+++ unit7/ServerLog.java
@@ -2,15 +2,19 @@ package unit7;
 
 import java.io.*;
 import java.net.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ServerLog {
     public static Logger infoLogger = Logger.getLogger("BasicInfo");
     public static Logger errorLogger = Logger.getLogger("Error Message");
+
     public static void main(String[] args) {
         try {
             ServerSocket server = new ServerSocket(13);
             Socket connection = server.accept();
+            infoLogger.log(Level.INFO, "Connection Established with a client");
+            // System.out.println("Connection established");
             connection.close();
             server.close();
         } catch (IOException e) {

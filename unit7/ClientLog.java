067a9f55c84099a89dc08c8a89bd32b838e01bf2
Sujan Rana <khembikram1@gmail.com>
2024-07-24 06:56:43 +0545

    communication update

--- unit7/ClientLog.java
+++ unit7/ClientLog.java
@@ -1,14 +1,25 @@
 package unit7;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.logging.Level;
 
 public class ClientLog {
     public static void main(String[] args) {
+        int ch;
         try {
-            Socket client = new Socket("localhost", 13);
-            client.close();
+            Socket socket = new Socket("localhost", 13);
+            socket.setSoTimeout(15000);
+            System.out.println(socket.toString());
+            InputStream in = socket.getInputStream();
+            // InputStreamReader reader = new InputStreamReader(in, 'ASCII');
+            while ((ch = in.read()) != -1) {
+                System.out.println((char) ch);
+            }
+            in.close();
+            socket.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }

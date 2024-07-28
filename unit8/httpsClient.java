0c3ff52303f8b1f4aed224ced94ff660e203ce70
Sujan Rana <khembikram1@gmail.com>
2024-07-28 07:06:28 +0545

    http Client

--- unit8/httpsClient.java
+++ unit8/httpsClient.java
@@ -1,6 +1,23 @@
 // Implement secure client socket using secure socket factory
 package unit8;
 
+import java.io.*;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
 public class httpsClient {
-       
+    public static void main(String[] args) {
+        SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket socket = null;
+        try {
+            socket = (SSLSocket) factory.createSocket("www.usps.com", 443);
+            String[] Suites = socket.getEnabledCipherSuites();
+            for (String string : Suites) {
+                System.out.println(string);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
